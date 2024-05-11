0c0f778b9964f1551ec6c31d879bd441584b6b59
jtaren <arenjatau@gmail.com>
2024-05-11 03:46:55 +0100

    flaskblog

--- Flask_blog/flaskblog.py
+++ Flask_blog/flaskblog.py
@@ -1,11 +1,11 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 posts = [
     {
-        'authors': 'Corey Schafer',
+        'author': 'Corey Schafer',
         'title': 'Blog Post',
         'content': 'First post content',
         'date_posted': 'April 20, 2018'
