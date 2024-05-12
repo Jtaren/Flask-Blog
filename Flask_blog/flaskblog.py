7ac6f7cc90d21d7ad0907a38ec9b5cb7648738b4
jtaren <arenjatau@gmail.com>
2024-05-12 09:41:00 +0100

    flaskblog

--- Flask_blog/flaskblog.py
+++ Flask_blog/flaskblog.py
@@ -1,17 +1,13 @@
-
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
-from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
-
-app.config['SECRET_kEY'] = '96b9dfdd948e108fd07bee8247e1516b'
-csrf = CSRFProtect(app)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
 posts = [
     {
         'author': 'Corey Schafer',
-        'title': 'Blog Post',
+        'title': 'Blog Post 1',
         'content': 'First post content',
         'date_posted': 'April 20, 2018'
     },
@@ -23,26 +19,38 @@ posts = [
     }
 ]
 
+
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template('home.html', posts=posts)
 
+
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
 
-@app.route("/register")
-@csrf.exempt
+
+@app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
-@app.route("/login")
-@csrf.exempt
+
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
